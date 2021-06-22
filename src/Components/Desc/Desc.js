140ee3c74bf9698f1c394db885b9ee4c2260f6c1
Jagal Sree <jagalsree007@gmail.com>
2021-06-22 05:52:11 +0530

    logo

--- src/Components/Desc/Desc.js
+++ src/Components/Desc/Desc.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Desc.css'
 import Card from '../UI/Card/Card'
-
+import Logo from '../Logo/Logo'
 function Desc() {
     return (
         <div>
-            <Card>Sree Jagath</Card>
+            <Card>
+                <div style={{padding:'50px 0'}}>
+                <Logo/>
+                </div>
+            </Card>
         </div>
     )
 }
---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Logo.css'
+function Logo() {
+    return (
+        <div className="logo">
+            <a href="#">Sree Jagath</a>
+        </div>
+    )
+}
+
+export default Logo
