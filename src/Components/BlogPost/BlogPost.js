922ee9ab2ecbf88aa1e74f1474ba9aee102744bd
Jagal Sree <jagalsree007@gmail.com>
2021-06-23 21:01:05 +0530

    img

--- src/Components/BlogPost/BlogPost.js
+++ src/Components/BlogPost/BlogPost.js
@@ -5,9 +5,13 @@ import Card from '../UI/Card/Card'
 function BlogPost() {
     return (
         
-            <Card width='70%'>
-            Blogpost
-            </Card>
+            <div className="blogPostContainer">
+                <Card>
+                    <span>Featured</span>
+                    <h1>Lorem ipsum dolor sit amet.</h1>
+                <span>Lorem ipsum dolor sit.</span>
+                </Card>
+            </div>
         
     )
 }
