678c518ec1190bfa8dc0112a1c2d171cef4bbfb8
Jagal Sree <jagalsree007@gmail.com>
2021-06-24 09:41:54 +0530

    sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,7 +4,7 @@ import Card from '../UI/Card/Card'
 function Sidebar() {
     return (
         <div className="sidebarContainer">
-        <Card style={{marginBottom:'20px'}}>
+        <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
             <div className="cardHeader">
                 <span>
                     About Us
@@ -18,11 +18,36 @@ function Sidebar() {
                    <p className="bio"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci ratione ipsam placeat quisquam illo amet odio, cumque ipsum beatae magnam eveniet officiis commodi quia minus veritatis voluptatibus libero nulla atque id, nisi vitae! Officiis quidem repudiandae aliquam non doloremque.</p>
                 </div>
         </Card>
-        <Card>
+        <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
             <div className="cardHeader">
                 <span>
                     Social Network
                 </span>
+            </div>
+        </Card>
+        <Card style={{marginBottom:'20px',padding:'20px',boxSizing:'border-box'}}>
+            <div className="cardHeader">
+                <span>
+                    Recent Posts
+                </span>
+            </div>
+            <div className="recentPosts">
+                <div className="recentPost">
+                    <h3>Post Title</h3>
+                    <span>April 28,2021</span>
+                </div>
+                <div className="recentPost">
+                    <h3>Post Title</h3>
+                    <span>April 28,2021</span>
+                </div>
+                <div className="recentPost">
+                    <h3>Post Title</h3>
+                    <span>April 28,2021</span>
+                </div>
+                <div className="recentPost">
+                    <h3>Post Title</h3>
+                    <span>April 28,2021</span>
+                </div>
             </div>
         </Card>
         </div>
